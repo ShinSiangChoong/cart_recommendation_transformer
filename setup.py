da3c351acc02e0c661fe1ae550f04a2f3ca17ab2
shinsiang <css919@hotmail.com>
2023-03-29 19:17:12 +0800

    Make src a module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description="A recommendation model recommending items based on what currently in a user's cart",
+    author='ShinSiang Choong',
+)
