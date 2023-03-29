c4a75f5797fe93750872b3c49cbedbd001615895
shinsiang <css919@hotmail.com>
2023-03-30 02:19:56 +0800

    Add doc strings and comments

--- src/cart_dataset.py
+++ src/cart_dataset.py
@@ -6,17 +6,27 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class CartDataset(Dataset):
+    """ 
+    Pytorch Dataset to represent the items in the carts
+    Unknown/padded items/categories are represented using 0
+    Args:
+        df_cart (pd.DataFrame): a cart level dataframe
+        df_cart_item (pd.DataFrame): a cart-item pair dataframe
+        n_train_items (int): number of item for training
+        max_items_per_cart (int): maximum number of items per cart
+        is_train (bool): training if true else evaluating
+    """
     def __init__(
         self, 
         df_cart, 
         df_cart_item, 
         n_train_items,
-        max_items, 
+        max_items_per_cart, 
         is_train=True, 
     ):
         self.df_cart = df_cart
         self.df_cart_item = df_cart_item
-        self.max_items = max_items
+        self.max_items_per_cart = max_items_per_cart
         self.n_train_items = n_train_items
         self.is_train = is_train
         
@@ -28,26 +38,25 @@ class CartDataset(Dataset):
         item_cats = self.df_cart_item.loc[
             event_id, ['product_idx', 'category_idx']
         ]
-        if self.is_train and len(item_cats) > self.max_items:
-            item_cats = item_cats.sample(self.max_items)
+        if self.is_train and len(item_cats) > self.max_items_per_cart:
+            item_cats = item_cats.sample(self.max_items_per_cart)
         items = item_cats['product_idx'].tolist()
         cats = item_cats['category_idx'].tolist()
         
         n_items = len(items)
         mask = [1]*n_items
         if self.is_train:
-            # mask_idx = np.random.choice(n_items)
-            mask_idx = n_items - 1
-            pad_len = self.max_items - n_items
+            mask_idx = np.random.choice(n_items) # mask a random item
+            # mask_idx = n_items - 1  # mask only last item
+            pad_len = self.max_items_per_cart - n_items
             att_mask = [0]*n_items + [1]*pad_len
             items += [0]*pad_len
             cats += [0]*pad_len
             mask += [0]*pad_len
         else:
             att_mask = [0]*len(items)
-            # mask_idx = idx % n_items
-            # mask_idx = 0
-            mask_idx = n_items - 1
+            # mask_idx = idx % n_items  # mask deterministically for evaluation 
+            mask_idx = n_items - 1  # mask last item for evaluation
             
         mask[mask_idx] = 0
         mask = torch.BoolTensor(mask)
@@ -56,7 +65,7 @@ class CartDataset(Dataset):
         target = torch.clip(torch.LongTensor(items) - 1, min=0)
         att_mask = torch.BoolTensor(att_mask)
         
-        # use 0 for unseen item
+        # use 0 for items not in the training set
         if not self.is_train:
             src *= (src <= self.n_train_items) 
             
@@ -64,6 +73,14 @@ class CartDataset(Dataset):
     
     
 class CartDatasetProd(Dataset):
+    """ 
+    Pytorch Dataset to represent the items in the carts (for production)
+    Unknown/padded items/categories are represented using 0
+    Items/Categories that do not exist during training are treated as unknowns
+    Args:
+        df_cart (pd.DataFrame): a cart level dataframe
+        df_cart_item (pd.DataFrame): a cart-item pair dataframe
+    """
     def __init__(
         self, 
         df_cart, 
--- src/model.py
+++ src/model.py
@@ -4,6 +4,14 @@ import torch.nn.functional as F
 
 
 class Model(nn.Module):
+    """ 
+    A transformer model for completing item sets
+    Args:
+        n_items (int): number of unique items (excluding unknowns)
+        n_cats (int): number of unique item categories (excluding unknowns)
+        item_emb_dim (int): item embedding dimension
+        cat_emb_dim (int): category embedding dimension
+    """
     def __init__(self, n_items, n_cats, item_emb_dim, cat_emb_dim):
         super(Model, self).__init__()
         self.item_emb = nn.Embedding(n_items+1, item_emb_dim)
@@ -11,7 +19,9 @@ class Model(nn.Module):
         emb_dim = item_emb_dim+cat_emb_dim
         self.tfm = nn.TransformerEncoder(
             nn.TransformerEncoderLayer(
-                d_model=emb_dim, nhead=8, batch_first=True
+                d_model=emb_dim, 
+                nhead=8, 
+                batch_first=True
             ), 
             num_layers=6
         )
